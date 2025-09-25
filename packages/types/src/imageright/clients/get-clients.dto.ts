d80fb41d648ae084a61b4ca261c27c18f1ea5116
zz-koji <146907072+zz-koji@users.noreply.github.com>
2025-09-25 10:18:01 -0500

    get clients dto: added clientCode

--- packages/types/src/imageright/clients/get-clients.dto.ts
+++ packages/types/src/imageright/clients/get-clients.dto.ts
@@ -3,6 +3,7 @@ import { z } from 'zod/v4'
 export const getClientsDto = z.object({
 	clientId: z.coerce.number().optional().nullable(),
 	clientName: z.string().optional().nullable(),
+	clientCode: z.string().optional().nullable()
 })
 
 export type GetClientsDto = z.infer<typeof getClientsDto>
