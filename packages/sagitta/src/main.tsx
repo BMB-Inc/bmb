54cf51e77664773a67eef220e6b4c1bf590f1b73
adevries1022 <adevries@bmbinc.com>
2025-10-21 09:56:16 -0500

    Integrate NuqsAdapter into main application component
    - Wrapped the App component with NuqsAdapter to enhance functionality within the application.
    - Ensured compatibility with existing QueryClient and MantineProvider setup.

--- packages/sagitta/src/main.tsx
+++ packages/sagitta/src/main.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import '@mantine/core/styles.css'
+import { NuqsAdapter } from 'nuqs/adapters/react';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -20,7 +21,9 @@ createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
       <MantineProvider>
         <BrowserRouter>
-            <App />
+        <NuqsAdapter>
+          <App />
+        </NuqsAdapter>
         </BrowserRouter>
       </MantineProvider>
     </QueryClientProvider>
