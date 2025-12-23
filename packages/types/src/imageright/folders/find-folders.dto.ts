4a486103d40776b019921b4f6a185f0bd877e3f8
zz-koji <banderson@bmbinc.com>
2025-12-23 15:23:35 -0600

    pc

--- packages/types/src/imageright/folders/find-folders.dto.ts
+++ packages/types/src/imageright/folders/find-folders.dto.ts
@@ -8,8 +8,8 @@ const folderAttributeFilterSchema = z.object({
 
 export const findFoldersBodyDto = z.object({
   Description: z.string().optional().nullable(),
-  IsDeleted: z.boolean(),
-  FileId: z.coerce.number().optional().nullable(),
+  IsDeleted: z.boolean().optional(),
+  FileId: z.coerce.number(),
   ParentId: z.coerce.number().optional().nullable(),
   FolderTypeIds: z.array(z.coerce.number()).optional(),
   Attributes: z.array(folderAttributeFilterSchema).optional(),
