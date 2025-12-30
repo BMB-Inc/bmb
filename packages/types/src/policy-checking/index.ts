96ebb0e7a118df3a18519ad8305c26a08dc9e3d1
zz-koji <banderson@bmbinc.com>
2025-12-30 14:16:55 -0600

    templating

--- packages/types/src/policy-checking/index.ts
+++ packages/types/src/policy-checking/index.ts
@@ -2,4 +2,5 @@ export * from './request.schema';
 export * from './document.schema';
 export * from './email.schema';
 export * from './email-template.schema';
+export * from './email-template-token.schema';
 export * from './response.schema';
