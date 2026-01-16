1d3df7bb178ea036984d0c027d5482fa6042533e
zz-koji <banderson@bmbinc.com>
2026-01-16 08:53:57 -0600

    get client by id

--- packages/types/src/policy-checking/client-code.schema.ts
+++ packages/types/src/policy-checking/client-code.schema.ts
@@ -1,8 +1,11 @@
 import z from 'zod/v4';
 
+import { imagerightClientSchema } from '../imageright/clients/clients.schema';
+
 export const policyCheckClientCodeSchema = z.object({
   clientId: z.number().int(),
   clientCode: z.string().nullable(),
+  client: imagerightClientSchema.nullable(),
 });
 
 export type PolicyCheckClientCode = z.infer<typeof policyCheckClientCodeSchema>;
